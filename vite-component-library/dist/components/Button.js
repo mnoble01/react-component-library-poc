b6f1878db60a6b6642594b10474025aead3d6f43
mnoble01 <mnoble@salsify.com>
2026-01-14 13:27:22 -0600

    Back to using preserveModules (I think the rollupOptions.external is correct now

--- vite-component-library/dist/components/Button.js
+++ vite-component-library/dist/components/Button.js
@@ -1,5 +1,5 @@
 import { jsx as t, Fragment as l } from "react/jsx-runtime";
-import r from "./Button.module.js";
+import r from "./Button.module.css.js";
 function u({ type: n, label: e, onClick: o, className: i }) {
   return /* @__PURE__ */ t(l, { children: /* @__PURE__ */ t("div", { className: i, children: /* @__PURE__ */ t("button", { className: `${r.button} ${r[n]}`, onClick: o, children: e }) }) });
 }
--- vite-component-library/vite.config.ts
+++ vite-component-library/vite.config.ts
@@ -1,40 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'node:path'
 import { glob } from 'glob'
-import { fileURLToPath } from 'node:url';
 
 // https://vite.dev/config/
-console.log(glob.sync('./src/**/*.{ts,tsx}'));
 export default defineConfig({
   plugins: [react()],
   build: {
     cssCodeSplit: true,
     lib: {
-      entry: Object.fromEntries(
-        glob.sync('./src/components/*.{ts,tsx,css}').map((file) => {
-          // This removes `src/` as well as the file extension from each
-          // file, so e.g. src/nested/foo.js becomes nested/foo
-          return [path.relative('src', file.slice(0, file.length - path.extname(file).length)), 
-            // This expands the relative paths to absolute paths, so e.g.
-            // src/nested/foo becomes /project/src/nested/foo.js
-            fileURLToPath(new URL(file, import.meta.url))
-          ];
-        })
-      ),
-      // The following outputs files to dist as a flat structure: e.g. dist/Button.js
-      // and using preserveModules introduces other problems as noted below
-      // entry: glob.sync('./src/**/*.{ts,tsx}'),
+      // Option 1: Explicitly list each entry point and transform the file path
+      // entry: Object.fromEntries(
+      //   glob.sync('./src/components/*.{ts,tsx,css}').map((file) => {
+      //     // This removes `src/` as well as the file extension from each
+      //     // file, so e.g. src/nested/foo.js becomes nested/foo
+      //     return [path.relative('src', file.slice(0, file.length - path.extname(file).length)), 
+      //       // This expands the relative paths to absolute paths, so e.g.
+      //       // src/nested/foo becomes /project/src/nested/foo.js
+      //       fileURLToPath(new URL(file, import.meta.url))
+      //     ];
+      //   })
+      // ),
+      // Option 2: Using the following glob requires setting preserveModules=true, preserveModulesRoot='src' below
+      entry: glob.sync('./src/components/*.{ts,tsx,css}'),
       formats: ['es'],
     },
     rollupOptions: {
       // The following prevents bundling react into the library build
       external: ['react', 'react-dom', 'react/jsx-runtime', 'react-dom/client'],
       output: {
-        // The following outputs absolute paths with dist: dist/Users/mnoble/dev/react-component-library/vite-component-library/src/components/Button.tsx
-        // preserveModules: true,
-        // This helps, but dist still contains react/react-dom output: dist/Users/mnoble/dev/react-component-library/vite-component-library/node_modules/react/cjs/react-jsx-runtime.development.js 
-        // preserveModulesRoot: 'src',
+        preserveModules: true,
+        preserveModulesRoot: 'src',
       },
     },
   },
