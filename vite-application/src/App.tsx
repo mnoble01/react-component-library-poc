69f7f3beab907a2f6c66ae775f2b69650bdc66ac
mnoble01 <mnoble@salsify.com>
2026-01-14 17:18:32 -0600

    move vite-application to toplevel dir

---
+++ vite-application/src/App.tsx
@@ -0,0 +1,28 @@
+import './App.css'
+// import RslibButton from '@mnoble01/rslib-component-library/components/Button'
+import ViteButton from '@mnoble01/vite-component-library/components/Button'
+
+function App() {
+  return (
+    <>
+      <h1>Vite Component Library</h1>
+      <div className="card">
+        <ViteButton
+          type="primary"
+          label="Vite Primary Button"
+          onClick={() => console.log('Vite Button Clicked')}
+        />
+      </div>
+      {/* <h1>Rslib Component Library</h1>
+      <div className="card">
+        <RslibButton
+          type="primary"
+          label="Rslib Primary Button"
+          onClick={() => console.log('Rslib Button Clicked')}
+        />
+      </div> */}
+    </>
+  )
+}
+
+export default App
