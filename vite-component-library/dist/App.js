02ecfe2d208d56adcbc442eacee1904c27b2754b
mnoble01 <mnoble@salsify.com>
2026-01-14 11:57:58 -0600

    Add Button component

---
+++ vite-component-library/dist/App.js
@@ -0,0 +1,5 @@
+import "./jsx-runtime-CG-hOSXf.js";
+import { A as a } from "./App-B3_zMr3e.js";
+export {
+  a as default
+};
--- vite-component-library/vite.config.ts
+++ vite-component-library/vite.config.ts
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { glob } from 'glob'
+// import packageJson from './package.json'
+
+// const packageName = packageJson.name.split('/').pop() || packageJson.name
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: glob.sync(path.resolve(__dirname, 'src/**/*.{ts,tsx}')),
+      formats: ['es'],
+      // name: camelCase(packageName, { pascalCase: true }),
+      // fileName: packageName,
+    },
+  },
 })
