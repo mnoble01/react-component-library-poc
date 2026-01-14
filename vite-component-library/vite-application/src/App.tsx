55140950dd9ae7f4e993fd5e02febcbeecd8ddf2
mnoble01 <mnoble@salsify.com>
2026-01-14 17:07:12 -0600

    import components - getting an error: 5:04:15 PM [vite] Internal server error: Missing "./components/Button" specifier in "@mnoble01/vite-component-library" package

--- vite-component-library/vite-application/src/App.tsx
+++ vite-component-library/vite-application/src/App.tsx
@@ -1,33 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+// import RslibButton from '@mnoble01/rslib-component-library/components/Button';
+import ViteButton from '@mnoble01/vite-component-library/components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Vite Component Library</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <ViteButton
+          type="primary"
+          label="Vite Primary Button"
+          onClick={() => console.log('Rslib Primary Button Clicked')}
+        />
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Rslib Component Library</h1>
+      <div className="card">
+        <RslibButton
+          type="primary"
+          label="Rslib Primary Button"
+          onClick={() => console.log('Rslib Primary Button Clicked')}
+        />
+      </div>
     </>
   )
 }
