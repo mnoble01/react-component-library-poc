115d401d3813d9ba81aa447a95150ac6789d0a07
mnoble01 <mnoble@salsify.com>
2026-01-15 09:46:18 -0600

    oopsgst

--- vite-application/src/App.tsx
+++ vite-application/src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css'
-// import RslibButton from '@mnoble01/rslib-component-library/components/Button'
 import ViteButton from '@mnoble01/vite-component-library/components/Button'
+import RslibButton from '@mnoble01/rslib-component-library/components/Button'
 
 function App() {
   return (
@@ -13,14 +13,14 @@ function App() {
           onClick={() => console.log('Vite Button Clicked')}
         />
       </div>
-      {/* <h1>Rslib Component Library</h1>
+      <h1>Rslib Component Library</h1>
       <div className="card">
         <RslibButton
           type="primary"
           label="Rslib Primary Button"
           onClick={() => console.log('Rslib Button Clicked')}
         />
-      </div> */}
+      </div>
     </>
   )
 }
